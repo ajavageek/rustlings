05a8599dbb267124e1e435a407e532cd585181e2
Nicolas Frankel <nicolas@frankel.ch>
2021-03-08 12:13:47 +0100

    Finish the solving of the rest of the exercises

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -23,6 +22,18 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            Err("String is empty".to_owned())
+        } else {
+            let items: Vec<&str> = s.split(",").collect();
+            let name = items[0].to_string();
+            if name.is_empty() {
+                Err("Name is empty".to_owned())
+            } else {
+                let age = (items[1] as &str).parse::<usize>().unwrap();
+                Ok(Person { name, age })
+            }
+        }
     }
 }
 
